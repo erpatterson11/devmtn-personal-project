8a8688f5cf0479128289f5d5b2c94ffa261f8468
Eric Patterson <erpat@Window-To-Erics-Soul.(none)>
2017-07-21 23:04:41 -0500

    improving animation performance

--- dist/bundle.js
+++ dist/bundle.js
@@ -149,9 +149,22 @@ angular.module("portfolioApp").service("reusableFuncsService", ["$http", functio
 // }
 angular.module('portfolioApp').controller('aboutCtrl', ["$scope", "aboutService", function($scope, aboutService) {
 
+    $scope.iconColors = {}
 
     $scope.images = aboutService.images
-    
+
+    $scope.toggleBoxShadow = function(item, bool) {
+        if (bool) {
+          $scope.iconColors[item.key] = {}
+          $scope.iconColors[item.key].dropShadow = {"filter": "drop-shadow(0 0 25px " + item.value.color +")"} 
+          $scope.iconColors[item.key].textShadow = {"textShadow": "0 0 5px " + item.value.color}           
+        } else {
+        $scope.iconColors[item.key].dropShadow = null
+        $scope.iconColors[item.key].textShadow = null
+        }
+    }
+
+    // aboutService.pulseNeon()
 }])
 angular.module('portfolioApp').service('aboutService', function() {
 
@@ -165,6 +178,7 @@ angular.module('portfolioApp').service('aboutService', function() {
         this.postgresql = new Image()
         this.sass = new Image()
         this.jquery = new Image()
+        this.greensock = new Image()
         this.webpack = new Image()
         this.gulp = new Image()
         this.git = new Image()
@@ -179,18 +193,62 @@ angular.module('portfolioApp').service('aboutService', function() {
         this.postgresql.src = './app/routes/about/images/postgresql.svg'
         this.sass.src = './app/routes/about/images/sass.svg'
         this.jquery.src = './app/routes/about/images/jquery.svg'
+        this.greensock.src = './app/routes/about/images/greensock.svg'
         this.webpack.src = '/app/routes/about/images/webpack.svg'
         this.gulp.src = './app/routes/about/images/gulp.svg'
         this.git.src = './app/routes/about/images/git.svg'
         this.github.src = './app/routes/about/images/github_logo.svg'
+
+        this.html.color = '#e44d26'
+        this.css.color = '#1572b6'
+        this.javascript.color = '#f0db4f'       
+        this.react.color = '#61dafb'
+        this.angular.color = '#c4473a'
+        this.node.color = '#83cd29'
+        this.postgresql.color = '#336791'
+        this.sass.color = '#cb6699'
+        this.jquery.color = '#0868ac'
+        this.greensock.color = '#8ac640'
+        this.webpack.color = '#1c78c0'
+        this.gulp.color = '#eb4a4b'
+        this.git.color = '#f34f29'
+        this.github.color = '#ffffff'
     }
 
     this.images = new Images()
 
+    this.pulseNeon = function() {
+        let workingNeon = document.querySelectorAll('.neon-animation')
+        let flickeringNeon = document.querySelector('#neon-flicker')
+        let tl = new TimelineMax({
+                        repeat:-1,
+                        yoyo: true
+                      })
+
+
+        let orange = "#ff9933"
+        let white = "#ffffff"
+        
+        tl.to(workingNeon, 0.2, {
+            // "text-shadow": `0 0 10px ${white}, 0 0 20px ${white}, 0 0 30px ${white}, 0 0 40px ${orange}, 0 0 70px ${orange}, 0 0 80px ${orange}, 0 0 100px ${orange}, 0 0 150px ${orange}`,
+            "opacity" : 0.8
+        }).to(workingNeon, 0.4, {
+            // "text-shadow": `0 0 5px ${white}, 0 0 10px ${white}, 0 0 15px ${white}, 0 0 20px ${orange}, 0 0 35px ${orange}, 0 0 40px ${orange}, 0 0 50px ${orange}, 0 0 750px ${orange}`,
+            "opacity" : 0.7
+        }).to(workingNeon, 0.1, {
+            // "text-shadow": `0 0 10px ${white}, 0 0 20px ${white}, 0 0 30px ${white}, 0 0 40px ${orange}, 0 0 70px ${orange}, 0 0 80px ${orange}, 0 0 100px ${orange}, 0 0 150px ${orange}`,
+            "opacity" : 0.8
+        }).to(workingNeon, 0.3, {
+            // "text-shadow": `0 0 5px ${white}, 0 0 10px ${white}, 0 0 15px ${white}, 0 0 20px ${orange}, 0 0 35px ${orange}, 0 0 40px ${orange}, 0 0 50px ${orange}, 0 0 750px ${orange}`,
+            "opacity" : 0.7
+        })
+
+        tl.play()
+        
+    }
+
+
 })
-
-
-
 angular.module("portfolioApp").controller("gameCtrl", ["$scope", "$timeout", "scoreService", "gameService", function($scope, $timeout, scoreService, gameService) {
 
 
@@ -1381,6 +1439,9 @@ angular.module("portfolioApp").service("scoreService", ["$http", function($http)
   }
 
 }]);
+
+
+
 
 // INITILIZE CONTROLLER
 // ============================================================
--- public/app/routes/about/aboutCtrl.js
+++ public/app/routes/about/aboutCtrl.js
@@ -1,6 +1,19 @@
 angular.module('portfolioApp').controller('aboutCtrl', function($scope, aboutService) {
 
+    $scope.iconColors = {}
 
     $scope.images = aboutService.images
-    
+
+    $scope.toggleBoxShadow = function(item, bool) {
+        if (bool) {
+          $scope.iconColors[item.key] = {}
+          $scope.iconColors[item.key].dropShadow = {"filter": "drop-shadow(0 0 25px " + item.value.color +")"} 
+          $scope.iconColors[item.key].textShadow = {"textShadow": "0 0 5px " + item.value.color}           
+        } else {
+        $scope.iconColors[item.key].dropShadow = null
+        $scope.iconColors[item.key].textShadow = null
+        }
+    }
+
+    // aboutService.pulseNeon()
 })
