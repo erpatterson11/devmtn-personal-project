de03ee00af49cfa11d412d272b3fb7bbf91963ee
Eric Patterson <erpat@Window-To-Erics-Soul.(none)>
2017-08-08 18:09:59 -0500

    updated svg button directive to alert when accessing a desktop only project via touch

--- public/app/directives/svgButton/svgButtonDir.js
+++ public/app/directives/svgButton/svgButtonDir.js
@@ -1,10 +1,28 @@
-angular.module('portfolioApp').directive('svgButton', function() {
+angular.module('portfolioApp').directive('svgButton', function($state) {
 
     return {
         restrict: 'E',
         templateUrl: './app/directives/svgButton/svgButtonTmpl.html',
         scope: {
-            text: '@'
+            text: '@',
+            desktop: '@',
+            toState: '@'
+        },
+        link: function(scope, elem, attrs) {
+            elem.on('touchstart', function() {
+                if (attrs.desktop !== undefined){
+                   if (confirm('Heads up, this project contains features that require keyboard input and is not yet optimized for mobile. Please visit on a laptop/desktop for the best experience.') ) {
+                       if (attrs.uiSref) $state.transitionTo(attrs.uiSref)
+                       else if (attrs.href) open(attrs.href)
+                   }    
+                }
+            })
+            elem.on('click', function() {
+                if (attrs.href) open(attrs.href)
+            })
+        },
+        controller: function() {
+
         }
     }
 })
